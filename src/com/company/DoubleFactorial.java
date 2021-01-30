50049dbe4b489d43cdeb2b439e9c4c598e214266
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-31 00:12:53 +0300

    Update DoubleFactorial.java

--- src/com/company/DoubleFactorial.java
+++ src/com/company/DoubleFactorial.java
@@ -16,37 +16,15 @@ import java.util.Scanner;
 class DoubleFactorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(calcDoubleFactorial(scanner.nextInt()));
+        System.out.println(calcDoubleFactorial(scanner.nextBigInteger()));
     }
 
-    public static BigInteger calcDoubleFactorial(int n) {
-        if (n == 0 || n == 1) {
-            return BigInteger.ONE;
-        }
-        boolean isEven = n % 2 == 0 ? true : false;
-        BigInteger a = BigInteger.valueOf(n);
-        if (isEven) {
-            return calcEvenDoubleFactorial(a);
-        } else {
-            return calcOddDoubleFactorial(a);
-        }
-    }
-
-    public static BigInteger calcEvenDoubleFactorial(BigInteger n) {
-        int res = n.compareTo(BigInteger.TWO);
-        if (res == 0 || res == -1) {
-            return BigInteger.TWO;
-        } else {
-            return n.multiply(calcEvenDoubleFactorial(n.subtract(BigInteger.TWO)));
-        }
-    }
-
-    public static BigInteger calcOddDoubleFactorial(BigInteger n) {
-        int res = n.compareTo(BigInteger.ONE);
+    public static BigInteger calcDoubleFactorial(BigInteger number) {
+        int res = number.compareTo(BigInteger.ONE);
         if (res == 0 || res == -1) {
             return BigInteger.ONE;
         } else {
-            return n.multiply(calcOddDoubleFactorial(n.subtract(BigInteger.TWO)));
+            return number.multiply(calcDoubleFactorial(number.subtract(BigInteger.TWO)));
         }
     }
 }
